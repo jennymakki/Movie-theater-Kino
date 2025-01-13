23882e4f52483e882988ca81566bdf733e9cd716
jennymakki <jjennyeklundd@hotmail.com>
2025-01-13 17:40:29 +0100

    created a server

---
+++ server.js
@@ -0,0 +1,35 @@
+// const express = require('express');
+// const path = require('path');
+
+import express from 'express' 
+import path from 'path'
+import fs from 'fs/promises'
+
+const app = express();
+const PORT = 3000;
+
+app.get ('/about-us.html', async (request, response) => {
+    const buf = await fs.readFile ('./dist/about-us.html')
+    const html = buf.toString()
+    response.send(html) 
+})
+
+app.get ('/', async (request, response) => {
+    const buf = await fs.readFile ('./dist/index.html')
+    const html = buf.toString()
+    response.send(html) 
+})
+
+app.get ('/movies', async (request, response) => {
+    const buf = await fs.readFile ('./dist/movies.html')
+    const html = buf.toString()
+    response.send(html) 
+})
+
+// Serve static files from the "public" directory
+    app.use('/kino-bio-projekt', express.static('./dist')) 
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
